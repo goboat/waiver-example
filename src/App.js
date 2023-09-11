16e8eac47bd87fee5542acf10ff555319c055b9d
Elias Hjelm <elias@goboat.dk>
2023-09-11 12:24:44 +0200

    prepare example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          GoBoat Waivers Test - this is a website
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="/en/waivers/64f65e58fc41bd3b8ed3b685?bookingId=64f14860ec268c4053217b63"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Check out an example waiver
         </a>
       </header>
     </div>
